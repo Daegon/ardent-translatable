3f96310397bbf00f2f6a5fd832ba13be32f14b87
Max van der Stam <max@pixelindustries.com>
2015-02-03 17:13:25 +0100

    First commit

---
+++ src/mvdstam/ArdentTranslatable/ArdentTranslatable.php
@@ -0,0 +1,46 @@
+<?php
+  
+  use LaravelBook\Ardent;
+  use Dimsav\Translatable\Translatable;
+
+  abstract class ArdentTranslatable extends Ardent {
+
+    use Translatable {
+      Translatable::save as translatableSave;
+    }
+
+    public function save(array $rules = array(),
+    array $customMessages = array(),
+    array $options = array(),
+    Closure $beforeSave = null,
+    Closure $afterSave = null)
+    {
+    if ($this->translatableSave($options))
+    {
+    return parent::save($rules, $customMessages, $options, $beforeSave, $afterSave);
+    }
+    return false;
+    }
+
+    public function getDirty() {
+      $data = parent::getDirty();
+
+      foreach($this->translations() as $translations) {
+        $data += $translations->getDirty();
+      }
+
+      return $data;
+    }
+
+    public function getAttributes() {
+        $data = parent::getAttributes();
+
+        if ($this->translatedAttributes) {
+          foreach($this->translatedAttributes as $field) {
+            $data[$field] = $this->$field;
+          }
+        }
+
+        return $data;
+    }
+  }
